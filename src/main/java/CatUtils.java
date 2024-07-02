d7753207939d722435c18c794df33931f8b5d942
Shubham Rajput <rajput.shubham37@gmail.com>
2024-07-02 14:40:50 +0530

    blob creation

---
+++ src/main/java/CatUtils.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.zip.InflaterInputStream;
+
+public class CatUtils {
+
+    private CatUtils(){
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void createCatFile(String filename){
+        String directoryHash = filename.substring(0, 2);
+        String fileHash = filename.substring(2, filename.length());
+
+        File blobFile = new File("./.git/objects/" + directoryHash + "/" + fileHash);
+        //creates a new file with a given path name which is then converted into abstract path
+
+        try {
+            String blob = new BufferedReader(new InputStreamReader
+                    (new InflaterInputStream(new FileInputStream(blobFile)))).readLine();
+            String content = blob.substring(blob.indexOf("\0") + 1);
+            System.out.println(content);
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,22 +10,40 @@ public class Main {
     // Uncomment this block to pass the first stage
     //
      final String command = args[0];
-
+     //this reads the first input in the command line
      switch (command) {
+       //if the first input is "init", then it means we need to create directory
        case "init" -> {
+         //to create a directory, we need a root file
          final File root = new File(".git");
+         //root file then now has multiple sub-files --> objects, refs, head
          new File(root, "objects").mkdirs();
          new File(root, "refs").mkdirs();
          final File head = new File(root, "HEAD");
 
          try {
+           //now we want the system to check if the head file already exists in the system
+           //if it does not exist, only then file will be created
            head.createNewFile();
+
+           //head generally includes the metadata of the path therefore, we need to write that to
+           //the head file. This can be done using the "write" method, which takes "Path" object as
+           //input and bytes as input. Then it writes those bytes to that path.
+
+           //"toPath" also throws an exception in case of invalid directory is passed as "Path"
            Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
+
            System.out.println("Initialized git directory");
          } catch (IOException e) {
+
            throw new RuntimeException(e);
          }
        }
+       case "cat-file" -> {
+         String hashcode = args[2];
+         CatUtils.createCatFile(hashcode);
+       }
+       //if command is not "init"
        default -> System.out.println("Unknown command: " + command);
      }
   }
