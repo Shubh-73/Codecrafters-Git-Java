691149e0641c1b51f45e0506a22ca2b723e44dfe
Shubham Rajput <rajput.shubham37@gmail.com>
2024-07-03 06:24:53 +0530

    blob creation

--- src/main/java/BlobUtils.java
+++ src/main/java/BlobUtils.java
@@ -54,8 +54,9 @@ public class BlobUtils {
             String hex = Integer.toHexString(0xff & b);
             if(hex.length() == 1) hexString.append('0');
             hexString.append(hex);
-            return hexString.toString();
+
         }
+        return hexString.toString();
     }
 
     /*
