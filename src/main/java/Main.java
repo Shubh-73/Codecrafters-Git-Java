92486a0fa9e42c5a3c82202de4defe6bfa532be2
Shubham Rajput <rajput.shubham37@gmail.com>
2024-07-06 10:18:19 +0530

    write stupid test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -40,7 +40,7 @@ public class Main {
         File dir = new File(".");
         byte[] sha = TreeGitUtils.writeTree(dir);
         if (sha != null) {
-          System.out.println("Tree SHA-1: " + BlobUtils.toHexSHA(sha));
+          System.out.println( BlobUtils.toHexSHA(sha));
         }
       }
       default -> System.out.println("Unknown command: " + command);
